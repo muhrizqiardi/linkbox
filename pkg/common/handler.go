c2918ec520383f7e505f67e2fde79bde875b8795
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-09-09 21:30:22 +0700

    feat: implement creating user

--- pkg/common/handler.go
+++ pkg/common/handler.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/schema"
 	"github.com/muhrizqiardi/linkbox/linkbox/pkg/templates"
 	"github.com/muhrizqiardi/linkbox/linkbox/pkg/user"
 )
@@ -14,13 +15,25 @@ func HandleRegisterPage(lg *log.Logger) func(w http.ResponseWriter, r *http.Requ
 			lg.Println("failed to render page:", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-
 	}
 }
 
 func HandleCreateUser(lg *log.Logger, us user.Service) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// user, err := us.Create()
+		if err := r.ParseForm(); err != nil {
+			lg.Println("failed to parse form body:", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+		var payload user.CreateUserDTO
+		if err := schema.NewDecoder().Decode(&payload, r.PostForm); err != nil {
+			lg.Println("failed to decode form body into a struct:", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+
+		if _, err := us.Create(payload); err != nil {
+			lg.Println("failed to create user:", err)
+		}
+
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 }
