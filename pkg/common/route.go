0704d1b98db316450d233e9aea17c6251b2269e3
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-09-09 22:26:35 +0700

    feat: add log in handler and page to route

--- pkg/common/route.go
+++ pkg/common/route.go
@@ -4,14 +4,18 @@ import (
 	"log"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/muhrizqiardi/linkbox/linkbox/pkg/auth"
 	"github.com/muhrizqiardi/linkbox/linkbox/pkg/user"
 )
 
-func Route(lg *log.Logger, us user.Service) chi.Router {
+func Route(lg *log.Logger, us user.Service, as auth.Service) chi.Router {
 	r := chi.NewRouter()
 
 	r.Get("/register", HandleRegisterPage(lg))
-	r.Post("/register", HandleCreateUser(lg, us))
+	r.Post("/register", HandleCreateUser(lg, us, as))
+
+	r.Get("/log-in", HandleLogInPage(lg))
+	r.Post("/log-in", HandleAuthLogIn(lg, as))
 
 	return r
 }
