46b05e41b54d1a83a8b8fea2a2f398ba436344ab
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-09-10 07:34:09 +0700

    feat: pass user data from middleware

--- pkg/auth/middleware.go
+++ pkg/auth/middleware.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"log"
 	"net/http"
+
+	"github.com/muhrizqiardi/linkbox/linkbox/pkg/user"
 )
 
-func AuthMiddleware(lg *log.Logger, as Service) func(next http.Handler) http.Handler {
+func AuthMiddleware(lg *log.Logger, as Service, us user.Service) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			cookie, err := r.Cookie("token")
@@ -25,7 +27,13 @@ func AuthMiddleware(lg *log.Logger, as Service) func(next http.Handler) http.Han
 				MaxAge: 8 * 24 * 60 * 60,
 			}
 			http.SetCookie(w, &newCookie)
-			ctx := context.WithValue(r.Context(), "userId", parsedToken.UserID)
+
+			foundUser, err := us.GetOneByID(parsedToken.UserID)
+			if err != nil {
+				lg.Println("user not found, user not authenticated:", err)
+			}
+
+			ctx := context.WithValue(r.Context(), "user", foundUser)
 			next.ServeHTTP(w, r.WithContext(ctx))
 			return
 		})
